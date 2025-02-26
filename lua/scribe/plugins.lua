3476232a1af19fe3f6c3eef8500a94c40cfa0bfa
Kenneth Sheridan <kennethdsheridan@gmail.com>
2025-02-25 21:56:44 -0800

    Added Neogit as a plugin

--- lua/scribe/plugins.lua
+++ lua/scribe/plugins.lua
@@ -50,6 +50,23 @@ return {
     },
 
     -- ─────────────────────────────────────────────────────────────────────────────
+    -- NeoGit
+    -- ─────────────────────────────────────────────────────────────────────────────
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+            "ibhagwan/fzf-lua",      -- optional
+            "echasnovski/mini.pick", -- optional
+        },
+        config = true
+    },
+
+    -- ─────────────────────────────────────────────────────────────────────────────
     -- RUSTACEANVIM (Commented Out)
     -- ─────────────────────────────────────────────────────────────────────────────
     -- {
